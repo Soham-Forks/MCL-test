5fc14d2e6c6d57cd19cf7d0034d572b898dfe703
Bots.Business <git.sync@bots.business>
2023-09-21 06:05:49 +0000

    Bots.Business export for bot id: 927249

---
+++ commands/_del.js
@@ -0,0 +1,29 @@
+/*CMD
+  command: /del
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+User.setProperty({
+  name: "MembershipChecker_Data",
+  value: null,
+  user_id: 345926
+})
+
+User.setProperty({
+  name: "MembershipChecker_Data",
+  value: null,
+  user_id: 16686279
+})
+
+User.setProperty({
+  name: "MembershipChecker_Data",
+  value: null,
+  user_id: 14911426
+})
+
